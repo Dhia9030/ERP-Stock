dbdb9bacb5659d53a8189e90c592cf10ab72afd2
Yassine <ya.dahmoul@gmail.com>
2025-01-29 14:22:14 +0100

    Update .gitignore

---
+++ backend/Services/ConcreteServices/GetProductService.cs
@@ -0,0 +1,6 @@
+﻿namespace backend.Services.ConcreteServices;
+
+public class GetProductService
+{
+    
+}
---
+++ backend/Services/ServicesContract/IGetProductService.cs
@@ -0,0 +1,6 @@
+﻿namespace backend.Services.ServicesContract;
+
+public interface IGetProductService
+{
+    
+}
